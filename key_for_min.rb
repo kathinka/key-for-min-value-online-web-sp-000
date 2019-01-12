7cc1b188234d7b04676d3ae991260a666be89c17
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-01-12 21:59:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,21 +1,4 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
-name_hash.collect do |key, value|
-  min = name_hash.values[0]
-  if value < min
-  min = value
-  smallest_key = name_hash.key
-end
-puts " min til slutt"
-puts min
-puts "minste nøkkel til slutt"
-puts key
-smallest_key
-end
-end
 
 def key_for_min_value(name_hash)
   min_key = nil
